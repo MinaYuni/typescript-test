f527a7b1e193bd762b27349fa6d9c8c1b6785ffd
MinaYuni <minhanh.ledoan@gmail.com>
2024-03-25 15:32:46 +0100

    add reset time and css

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import "./index.css";
 
+let watchContainer = document.querySelector(".watch");
 let timeText = document.querySelector(".time");
+let modeText = document.querySelector(".currentMode");
 let modeButton = document.querySelector(".mode");
 let increaseButton = document.querySelector(".increase");
 let lightButton = document.querySelector(".light");
+let resetButton = document.querySelector(".reset");
 
 let currentTime = new Date();
 let hourIsEdited = false;
@@ -11,8 +14,10 @@ let minuteIsEdited = false;
 let hoursAdded = 0;
 let minutesAdded = 0;
 
+let isLightMode = true;
+
 /**
- * Update the text field with the current value
+ * Update the time text field with the current value
  */
 function updateTime() {
   currentTime = new Date();
@@ -22,18 +27,22 @@ function updateTime() {
 }
 
 /**
- * Edit the displayed time
+ * Change to edit mode for the time
  */
 function timeEdit() {
   if (!hourIsEdited && !minuteIsEdited) {
     hourIsEdited = true;
+    modeText.textContent = "Mode : Hour";
   } else if (hourIsEdited && !minuteIsEdited) {
     hourIsEdited = false;
     minuteIsEdited = true;
+    modeText.textContent = "Mode : Minute";
   } else {
     hourIsEdited = false;
     minuteIsEdited = false;
+    modeText.textContent = "Mode : Time";
   }
+  updateTime();
 }
 
 /**
@@ -47,7 +56,24 @@ function increaseTime() {
   }
 }
 
+/**
+ * Change light mode to dark mode and vice versa
+ */
+function changeLightMode() {
+  isLightMode = !isLightMode;
+}
+
+/**
+ * Reset actual time to current time
+ */
+function resetTime() {
+  hoursAdded = 0;
+  minutesAdded = 0;
+}
+
 modeButton.addEventListener("click", timeEdit);
 increaseButton.addEventListener("click", increaseTime);
+lightButton.addEventListener("click", changeLightMode);
+resetButton.addEventListener("click", resetTime);
 
 setInterval(updateTime, 1000);
