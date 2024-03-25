45894fe25abd83d6dc8aad56e7b68f214e8bbd20
MinaYuni <minhanh.ledoan@gmail.com>
2024-03-25 15:01:53 +0100

    add increase time

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import "./index.css";
-import { MyClass } from "./example-unit";
 
 let timeText = document.querySelector(".time");
 let modeButton = document.querySelector(".mode");
@@ -7,20 +6,48 @@ let increaseButton = document.querySelector(".increase");
 let lightButton = document.querySelector(".light");
 
 let currentTime = new Date();
+let hourIsEdited = false;
+let minuteIsEdited = false;
+let hoursAdded = 0;
+let minutesAdded = 0;
 
 /**
  * Update the text field with the current value
  */
 function updateTime() {
-  let currentTime = new Date();
+  currentTime = new Date();
+  currentTime.setHours(currentTime.getHours() + hoursAdded);
+  currentTime.setMinutes(currentTime.getMinutes() + minutesAdded);
   timeText.textContent = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
 }
 
 /**
  * Edit the displayed time
  */
-function timeEdit() {}
+function timeEdit() {
+  if (!hourIsEdited && !minuteIsEdited) {
+    hourIsEdited = true;
+  } else if (hourIsEdited && !minuteIsEdited) {
+    hourIsEdited = false;
+    minuteIsEdited = true;
+  } else {
+    hourIsEdited = false;
+    minuteIsEdited = false;
+  }
+}
+
+/**
+ * Increase the time by hour and minute
+ */
+function increaseTime() {
+  if (hourIsEdited) {
+    hoursAdded++;
+  } else if (minuteIsEdited) {
+    minutesAdded++;
+  }
+}
 
 modeButton.addEventListener("click", timeEdit);
+increaseButton.addEventListener("click", increaseTime);
 
-setInterval(updateTime, 10);
+setInterval(updateTime, 1000);
