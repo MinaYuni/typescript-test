432d76b22907f72bc41d6e2f680fed3c9e3041a8
MinaYuni <minhanh.ledoan@gmail.com>
2024-03-25 14:19:20 +0100

    add current time

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import './index.css';
-import { MyClass } from './example-unit';
+import "./index.css";
+import { MyClass } from "./example-unit";
 
-const a = new MyClass(2);
-console.log('number is', a.get());
+let timeText = document.querySelector(".time");
+let modeButton = document.querySelector(".mode");
+let increaseButton = document.querySelector(".increase");
+let lightButton = document.querySelector(".light");
+
+let currentTime = new Date();
+
+/**
+ * Update the text field with the current value
+ */
+function updateTime() {
+  let currentTime = new Date();
+  timeText.textContent = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+}
+
+/**
+ * Edit the displayed time
+ */
+function timeEdit() {}
+
+modeButton.addEventListener("click", timeEdit);
+
+setInterval(updateTime, 10);
